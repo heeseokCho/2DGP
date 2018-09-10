addc8b4e991fd2d2415122f8255ba1a115c131ba
Heeseok(2dGP) <dswill038@naver.com>
2018-09-10 15:11:07 +0900

    character move
    draw_now() left to right

--- Lecture03/character_moves.py
+++ Lecture03/character_moves.py
@@ -1,11 +1,24 @@
-from pico2d import *
+from pico2d import*
+import os
+
+os.chdir('C:\\Users\CHS\\Desktop\\조희석\\전공\\2D Git\\2018\\2018-2DGP\\Labs\\Lecture04')
+
 
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image ('character.png')
 
-# 여기를 채우세요.
+x = 0
+
+
+while(x <800):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x  = x  + 2
+
+    delay(0.01)
+
 
 close_canvas()
-
