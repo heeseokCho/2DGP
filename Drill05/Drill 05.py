412611bf26c2c9d3c97dd369ac77ce96405b4b36
Heeseok(2dGP) <dswill038@naver.com>
2018-09-17 18:10:09 +0900

    Drill 05 과제
    2015182039 조희석
    step 1

---
+++ Drill05/Drill 05.py
@@ -0,0 +1,58 @@
+from pico2d import*
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+point = ((203,535),(132,243),(535,470),(477,203),
+         (715,136),(316,225),(510,92),(692,518),
+         (682,336),(712,349))
+
+def goto_1():
+    pass
+
+def goto_2():
+    pass
+
+def goto_3():
+    pass
+
+def goto_4():
+    pass
+
+def goto_5():
+    pass
+
+def goto_6():
+    pass
+
+def goto_7():
+    pass
+
+def goto_8():
+    pass
+
+def goto_9():
+    pass
+
+def goto_0():
+    pass
+
+
+while True:
+
+
+    goto_1()
+    goto_2()
+    goto_3()
+    goto_4()
+    goto_5()
+    goto_6()
+    goto_7()
+    goto_8()
+    goto_9()
+    goto_0()
+
+
+close_canvas()
