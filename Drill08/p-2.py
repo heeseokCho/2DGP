ae843da328c9b03f1d8254471621fe1d6b7b8f6c
Heeseok(2dGP) <dswill038@naver.com>
2018-10-08 14:52:15 +0900

    백그라운드를 그림
    2015182039 조희석 commit 2

--- Drill08/p-2.py
+++ Drill08/p-2.py
@@ -22,8 +22,13 @@ frame = 0
 n = 0
 
 def draw_point(p):
-    pass
+    global fame,n
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH//2,KPU_HEIGHT//2)
 
+
+    update_canvas()
+    
 def draw_curve(n):
     pass
 
@@ -33,6 +38,7 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 while True:
+    draw_point((0,0))
     draw_curve(n)
 
     handle_events()
