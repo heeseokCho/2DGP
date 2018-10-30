e03c3fb97c96121da3d7b56013ed3fb37be6ed38
Heeseok(2dGP) <dswill038@naver.com>
2018-10-31 01:32:48 +0900

    class  LINK 수정

--- 프로젝트/FrameWork적용/class_LINK.py
+++ 프로젝트/FrameWork적용/class_LINK.py
@@ -287,46 +287,38 @@ class WinState:
         Link.image.clip_draw(Link.frame * SIZE, 0, SIZE, SIZE, Link.x, Link.y)
 
 
-#UP_DOWN,DOWN_DOWN,LEFT_DOWN,RIGHT_DOWN,\
-#UP_UP,DOWN_UP,LEFT_UP,RIGHT_UP,\
-#ATTACK_DOWN,ATTACK_UP,AIM_TIMER= range(11)
-
 next_state_table = {
-    IdleState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    IdleState:{UP_DOWN:RunState,DOWN_DOWN:RunState,LEFT_DOWN:RunState,RIGHT_DOWN:RunState,
+               UP_UP:IdleState,DOWN_UP:IdleState,LEFT_UP:IdleState,RIGHT_UP:IdleState,
+               ATTACK_DOWN:AimState,ATTACK_UP:IdleState},
 
-    RunState: {UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    RunState: {UP_DOWN:RunState,DOWN_DOWN:RunState,LEFT_DOWN:RunState,RIGHT_DOWN:RunState,
+               UP_UP:IdleState,DOWN_UP:IdleState,LEFT_UP:IdleState,RIGHT_UP:IdleState,
+               ATTACK_DOWN:AimState,ATTACK_UP:RunState},
 
-    AimState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    AimState:{UP_DOWN:AimState,DOWN_DOWN:AimState,LEFT_DOWN:AimState,RIGHT_DOWN:AimState,
+               UP_UP:AimState,DOWN_UP:AimState,LEFT_UP:AimState,RIGHT_UP:AimState,
+               ATTACK_DOWN:AimState,ATTACK_UP:IdleState,AIM_TIMER:AimState},
 
-    AimIdleState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    AimIdleState:{UP_DOWN:AimRunState,DOWN_DOWN:AimRunState,LEFT_DOWN:AimRunState,RIGHT_DOWN:AimRunState,
+               UP_UP:AimIdleState,DOWN_UP:AimIdleState,LEFT_UP:AimIdleState,RIGHT_UP:AimIdleState,
+               ATTACK_DOWN:AimIdleState,ATTACK_UP:IdleState,AIM_TIMER:AimIdleState},
 
-    AimRunState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    AimRunState:{UP_DOWN:AimRunState,DOWN_DOWN:AimRunState,LEFT_DOWN:AimRunState,RIGHT_DOWN:AimRunState,
+               UP_UP:AimIdleState,DOWN_UP:AimIdleState,LEFT_UP:AimIdleState,RIGHT_UP:AimIdleState,
+               ATTACK_DOWN:AimRunState,ATTACK_UP:ShootState,AIM_TIMER:AimRunState},
 
-    ShootState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    ShootState:{UP_DOWN:ShootState,DOWN_DOWN:ShootState,LEFT_DOWN:ShootState,RIGHT_DOWN:ShootState,
+               UP_UP:ShootState,DOWN_UP:ShootState,LEFT_UP:ShootState,RIGHT_UP:ShootState,
+               ATTACK_DOWN:ShootState,ATTACK_UP:IdleState,AIM_TIMER:ShootState},
 
-    ShootState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
+    DieState:{UP_DOWN:DieState,DOWN_DOWN:DieState,LEFT_DOWN:DieState,RIGHT_DOWN:DieState,
+               UP_UP:DieState,DOWN_UP:DieState,LEFT_UP:DieState,RIGHT_UP:DieState,
+               ATTACK_DOWN:DieState,ATTACK_UP:DieState,AIM_TIMER:DieState},
 
-    DieState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None},
-
-    WinState:{UP_DOWN:None,DOWN_DOWN:None,LEFT_DOWN:None,RIGHT_DOWN:None,
-               UP_UP:None,DOWN_UP:None,LEFT_UP:None,RIGHT_UP:None,
-               ATTACK_DOWN:None,ATTACK_UP:None,AIM_TIMER:None}
+    WinState:{UP_DOWN:WinState,DOWN_DOWN:WinState,LEFT_DOWN:WinState,RIGHT_DOWN:WinState,
+               UP_UP:WinState,DOWN_UP:WinState,LEFT_UP:WinState,RIGHT_UP:WinState,
+               ATTACK_DOWN:WinState,ATTACK_UP:WinState,AIM_TIMER:WinState}
 
 }
 
@@ -335,151 +327,45 @@ class LINK:
 
     def __init__(self):
         self.image = load_image('Standing.png')
-        self.state = STANDING
-        self.look = DOWN
-        self.aimStart = False
-        self.chargeStart = False
-        self.shootStart = False
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self,None)
         self.frame = 0
-        self.aimCnt = 0
-        self.chargeCnt = 0
-        self.shootCnt = 0
         self.x,self.y = WINX//2, WINY//2
         self.dir = DOWN
         self.timer = 0
         self.velocityX,self.velocityY = 0,0
         self.enable = False
 
-        #좌상우하
-        self.RectBody = [self.x-16,self.y+20,self.x+16,self.y-20]
-        self.RectFoot = [self.x - 8, self.y - 12, self.x + 8, self.y - 20]
+    def update_rect(self):
+        pass
 
-    def SetState(self,_state):
+    def draw_rect(self):
+        pass
 
-        self.state = _state
+    def shoot_arrow(self):
+        pass
 
-        if self.state == STANDING:
-            self.image =  load_image('Standing.png')
-        elif self.state == WALKING:
-            self.image = load_image('Walking.png')
-        elif self.state == AIMING:
-            self.image = load_image('Aiming.png')
-        elif self.state == AIMWALKING:
-            self.image = load_image('AimWalking.png')
-        elif self.state == SHOOTING:
-            self.image = load_image('Shooting.png')
-        elif self.state == DIEING:
-            self.image = load_image('Dieing.png')
-        elif self.state == AIMSTANDING:
-            self.image = load_image('AimStanding.png')
+    def add_event(self,event):
+        self.event_que.imsert(0,event)
 
-    def Draw(self):
-        #조준할때는 느리게 그림
-        if self.state == AIMING:
-            self.image.clip_draw((int(self.frame / 4)) * SIZE, self.look, SIZE, SIZE, self.x, self.y)
-        else:
-            self.image.clip_draw(self.frame * SIZE, self.look,
-                                SIZE, SIZE, self.x, self.y)
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self,event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self,event)
 
-    def DrawRectangle(self):
-        draw_rectangle(self.RectBody[0],self.RectBody[1],self.RectBody[2],self.RectBody[3])
-        draw_rectangle(self.RectFoot[0],self.RectFoot[1],self.RectFoot[2],self.RectFoot[3])
+        self.update_rect()
 
+    def draw(self):
+        self.cur_state.draw(self)
 
-    def Update(self):
-        #조준할때는 느리게 그림
-        if self.state == AIMING:
-            self.frame = (self.frame + 1) % (self.state[1]*4)
-        else:
-            self.frame = (self.frame + 1) % self.state[1]
-
-
-        self.x += self.dirX * 8
-        self.y += self.dirY * 8
-
-        self.RectBody[0] = self.x-16
-        self.RectBody[2] = self.x+16
-        self.RectBody[1] = self.y-20
-        self.RectBody[3] = self.y+20
-
-        self.RectFoot[0] = self.x-8
-        self.RectFoot[2] = self.x+8
-        self.RectFoot[1] = self.y-12
-        self.RectFoot[3] = self.y-20
-
-        self.x = clamp(SIZE/2,self.x,WINX-SIZE/2)
-        self.y = clamp(SIZE/2, self.y, WINY-SIZE/2-200)
-
-        #조준시작했는지 체크
-        self.AimCheck()
-        #발사시작했는지 체크
-        self.ShootCheck()
+        self.draw_rect()
 
 
 
-    def MoveToStopCheck(self):
-        if self.dirX == 0 and self.dirY == 0:
-            if self.aimCnt <= 3:
-                self.SetState(STANDING)
-            else:
-                self.SetState(AIMSTANDING)
-
-    def AimCheck(self):
-        if self.aimStart == True:
-            self.aimCnt += 1
-
-        if self.aimCnt > AIMING[1]:
-            self.SetState(AIMWALKING)
-            self.aimStart = False
-
-    def ShootCheck(self):
-        if self.shootStart == True:
-            Link.SetState(SHOOTING)
-
-            if self.shootCnt == 0:
-                #화살만듬
-                Arrow.append(ARROW())
-                Arrow[-1].Dir = Link.look
-
-            self.shootCnt += 1
-
-        if self.shootCnt > SHOOTING[1]:
-            self.shootCnt = 0
-            self.shootStart = False
 
 
-            if self.dirX == 0 and self.dirY == 0:
-                self.SetState(STANDING)
-            else:
-                self.SetState(WALKING)
-            self.MoveToStopCheck()
 
-    def CheckAimComplete(self):
-        if self.aimCnt <= AIMING[1]:
-            self.SetState(WALKING)
-        else:
-            self.SetState(AIMWALKING)
-
-    def GoUp(self):
-        self.CheckAimComplete()
-
-        self.dirY += 1
-        self.look = UP
-
-    def GoDown(self):
-        self.CheckAimComplete()
-
-        self.dirY -= 1
-        self.look = DOWN
-
-    def GoLeft(self):
-        self.CheckAimComplete()
-
-        self.dirX -= 1
-        self.look = LEFT
-
-    def GoRight(self):
-        self.CheckAimComplete()
-
-        self.dirX += 1
-        self.look = RIGHT
