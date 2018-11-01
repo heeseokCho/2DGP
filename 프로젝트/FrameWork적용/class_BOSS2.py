474e7ef9a0434cf36290f15387291b13b2990534
Heeseok(2dGP) <dswill038@naver.com>
2018-11-01 22:59:44 +0900

    main_state2

--- 프로젝트/FrameWork적용/class_BOSS2.py
+++ 프로젝트/FrameWork적용/class_BOSS2.py
@@ -2,8 +2,8 @@ import game_framework
 from pico2d import*
 import game_world
 
-from class_BOSS1_BULLET1 import BOSS1_BULLET1
-from class_BOSS1_BULLET2 import BOSS1_BULLET2
+from class_BOSS2_BULLET1 import BOSS2_BULLET1
+from class_BOSS2_BULLET2 import BOSS2_BULLET2
 
 import math
 import random
@@ -14,7 +14,7 @@ SIZE = 64
 LEFT_TOP,LEFT,LEFT_BOTTOM,BOTTOM,\
 RIGHT_BOTTOM,RIGHT,RIGHT_TOP,TOP = range(8)
 
-# Boss1 Run Speed
+# Boss2 Run Speed
 PIXEL_PER_METER = (10.0/0.3)
 RUN_SPEED_KMPH = 20.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH*1000.0/60.0)
@@ -23,7 +23,7 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS*PIXEL_PER_METER)
 
 DEGREE_PER_TIME = PI
 
-# Boss1 Action Speed
+# Boss2 Action Speed
 TIME_PER_ACTION = 3
 ACTION_PER_TIME = 1.0/TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
@@ -34,35 +34,35 @@ next_state_table ={}
 
 class RunState:
     @staticmethod
-    def enter(Boss1,event):
-        Boss1.frame= 0
+    def enter(Boss2,event):
+        Boss2.frame= 0
 
     @staticmethod
-    def exit(Boss1,event):
+    def exit(Boss2,event):
         pass
 
     @staticmethod
-    def do(Boss1):
-        Boss1.frame = (Boss1.frame +FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time)%5
+    def do(Boss2):
+        Boss2.frame = (Boss2.frame +FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time)%4
 
-        Boss1.timer += get_time() - Boss1.cur_time
-        Boss1.cur_time = get_time()
+        Boss2.timer += get_time() - Boss2.cur_time
+        Boss2.cur_time = get_time()
 
-        if Boss1.timer >=5:
-            Boss1.shoot_bullet2()
-            Boss1.shoot_bullet1()
-            Boss1.timer = 0
+        if Boss2.timer >=5:
+            Boss2.shoot_bullet2()
+            Boss2.shoot_bullet1()
+            Boss2.timer = 0
 
     @staticmethod
-    def draw(Boss1):
-        Boss1.image.clip_draw(int(Boss1.frame)*SIZE*3,0,SIZE*3,SIZE*3,Boss1.x,Boss1.y)
+    def draw(Boss2):
+        Boss2.image.clip_draw(int(Boss2.frame)*SIZE*3,0,SIZE*3,SIZE*3,Boss2.x,Boss2.y)
 
 
-class BOSS1:
+class BOSS2:
 
     def __init__(self):
         self.x, self.y = WINX//2, WINY//2
-        self.image = load_image('Boss1.png')
+        self.image = load_image('Boss2.png')
         self.dir = 1
         self.velocity = 0
         self.frame = 0
@@ -73,14 +73,14 @@ class BOSS1:
         self.degree = 0
         self.timer = 0
 
-    #지뢰
+    #Link에게쏘는탄
     def shoot_bullet1(self):
-        bullet1 = BOSS1_BULLET1()
+        bullet1 = BOSS2_BULLET1()
         game_world.add_object(bullet1,1)
 
     #8방
     def shoot_bullet2(self):
-        bullet2 = [BOSS1_BULLET2(self.x,self.y,i) for i in range(8)]
+        bullet2 = [BOSS2_BULLET2(self.x,self.y,i) for i in range(8)]
 
         for o in bullet2:
             game_world.add_object(o,1)
