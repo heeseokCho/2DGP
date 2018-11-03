c172f3117aa009744d639acc9a178837bd58231b
Heeseok(2dGP) <dswill038@naver.com>
2018-11-03 16:35:47 +0900

    Boss2_Bullet2이미지수정

--- 프로젝트/FrameWork적용/class_BOSS2_BULLET2.py
+++ 프로젝트/FrameWork적용/class_BOSS2_BULLET2.py
@@ -8,6 +8,7 @@ import random
 
 WINX,WINY = 1600,1000
 SIZE  = 64
+PI = 3.141592
 
 LEFT_TOP,LEFT,LEFT_BOTTOM,BOTTOM,\
 RIGHT_BOTTOM,RIGHT,RIGHT_TOP,TOP = range(8)
@@ -18,37 +19,34 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH*1000.0/60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS*PIXEL_PER_METER)
 
+DEGREE_PER_TIME = PI
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0/TIME_PER_ACTION
+FRAMES_PER_ACTION = 1
+
 class BOSS2_BULLET2:
     image = None
 
     def __init__(self,x = WINX//2,y=WINY//2,dir=0):
         if BOSS2_BULLET2.image == None:
-            BOSS2_BULLET2.image = load_image('Enemy.png')
+            #BOSS2_BULLET2.image = load_image('Boss2_Bullet.png')
+            BOSS2_BULLET2.image = load_image('Boss2Bullet2.png')
+        self.frame = 0
         self.x,self.y = x,y
         self.dir = dir
         self.velocity = RUN_SPEED_PPS
+        self.degree = 0
 
 
     def draw(self):
-        if self.dir == LEFT_TOP:
-            BOSS2_BULLET2.image.clip_draw(SIZE*0,SIZE//2*2,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == LEFT:
-            BOSS2_BULLET2.image.clip_draw(SIZE*0,SIZE//2*1,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == LEFT_BOTTOM:
-            BOSS2_BULLET2.image.clip_draw(SIZE*0,SIZE//2*0,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == BOTTOM:
-            BOSS2_BULLET2.image.clip_draw(SIZE//2*1,SIZE//2*0,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == RIGHT_BOTTOM:
-            BOSS2_BULLET2.image.clip_draw(SIZE//2*2,SIZE//2*0,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == RIGHT:
-            BOSS2_BULLET2.image.clip_draw(SIZE//2*2,SIZE//2*1,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == RIGHT_TOP:
-            BOSS2_BULLET2.image.clip_draw(SIZE//2*2,SIZE//2*2,SIZE//2,SIZE//2,self.x,self.y)
-        elif self.dir == TOP:
-            BOSS2_BULLET2.image.clip_draw(SIZE//2*1,SIZE//2*2,SIZE//2,SIZE//2,self.x,self.y)
-
+        BOSS2_BULLET2.image.clip_composite_draw(int(self.frame) * SIZE // 2, 0, SIZE // 2, SIZE // 2,
+                                                math.radians(self.degree),'v', self.x, self.y, SIZE, SIZE)
+       #BOSS2_BULLET2.image.rotate_draw(math.radians(self.degree), self.x, self.y, SIZE, SIZE)
 
     def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
+
         if self.dir == LEFT_TOP:
             self.x -= self.velocity*game_framework.frame_time
             self.y += self.velocity*game_framework.frame_time
@@ -70,8 +68,10 @@ class BOSS2_BULLET2:
         elif self.dir == TOP:
             self.y += self.velocity*game_framework.frame_time
 
-        if self.x <main_state2.Circle.x-main_state2.Circle.r or self.x > main_state2.Circle.x+main_state2.Circle.r or \
-                self.y <main_state2.Circle.y-main_state2.Circle.r or self.y > main_state2.Circle.x+main_state2.Circle.r:
+        self.degree += DEGREE_PER_TIME
+
+        if self.x <0 or self.x > WINX or \
+                self.y <0 or self.y > WINY-250:
             game_world.remove_object(self)
 
 
